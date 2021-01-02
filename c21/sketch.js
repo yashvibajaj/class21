3daf2d2e1b7cd0cb8eddafe66867163fc4506172
yashvibajaj <76166448+yashvibajaj@users.noreply.github.com>
2021-01-02 17:54:35 +0530

    Add files via upload

---
+++ c21/sketch.js
@@ -0,0 +1,38 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  
+  if(bounceOff(movingRect,fixedRect)){
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
+  }
+  else{
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
+  drawSprites();
+}
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+  return true;
+}
+if (object1t.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+  return false;
+}  
+}
